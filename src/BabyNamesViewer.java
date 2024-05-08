77184a85a4974074544868503f0eec4199cd2ec5
Doga <doga@Dogas-MacBook-Air.local>
2024-05-09 00:11:44 +0300

    first

---
+++ src/BabyNamesViewer.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class BabyNamesViewer
+{  
+   public static void main(String[] args)
+   {  
+      JFrame frame = new BabyNamesFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
